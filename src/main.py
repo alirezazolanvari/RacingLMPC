fd7ed7d35bb2c7b17a7fd58fdcb21129e739b626
alirezazolanvari <alireza.zolanvari93@gmail.com>
2020-11-26 17:56:45 +0100

    some PEP8 warnings are fixed

--- src/main.py
+++ src/main.py
@@ -36,10 +36,11 @@ import numpy as np
 import pickle
 import pdb
 
+
 def main():
-    # ======================================================================================================================
-    # ============================================= Initialize parameters  =================================================
-    # ======================================================================================================================
+    # ==================================================================================================================
+    # ============================================= Initialize parameters  =============================================
+    # ==================================================================================================================
     N = 14                                    # Horizon length
     n = 6;   d = 2                            # State and Input dimension
     x0 = np.array([0.5, 0, 0, 0, 0, 0])       # Initial condition
@@ -54,21 +55,21 @@ def main():
     numSS_it, numSS_Points, Laps, TimeLMPC, QterminalSlack, lmpcParameters = initLMPCParams(map, N)
 
     # Init simulators
-    simulator     = Simulator(map)
-    LMPCsimulator = Simulator(map, multiLap = False, flagLMPC = True)
+    simulator = Simulator(map)
+    LMPCsimulator = Simulator(map, multiLap=False, flagLMPC=True)
 
-    # ======================================================================================================================
-    # ======================================= PID path following ===========================================================
-    # ======================================================================================================================
+    # ==================================================================================================================
+    # ======================================= PID path following =======================================================
+    # ==================================================================================================================
     print("Starting PID")
     # Initialize pid and run sim
     PIDController = PID(vt)
     xPID_cl, uPID_cl, xPID_cl_glob, _ = simulator.sim(xS, PIDController)
     print("===== PID terminated")
 
-    # ======================================================================================================================
-    # ======================================  LINEAR REGRESSION ============================================================
-    # ======================================================================================================================
+    # ==================================================================================================================
+    # ======================================  LINEAR REGRESSION ========================================================
+    # ==================================================================================================================
     print("Starting MPC")
     # Estimate system dynamics
     lamb = 0.0000001
@@ -80,34 +81,34 @@ def main():
     xMPC_cl, uMPC_cl, xMPC_cl_glob, _ = simulator.sim(xS, mpc)
     print("===== MPC terminated")
 
-    # ======================================================================================================================
-    # ===================================  LOCAL LINEAR REGRESSION =========================================================
-    # ======================================================================================================================
+    # ==================================================================================================================
+    # ===================================  LOCAL LINEAR REGRESSION =====================================================
+    # ==================================================================================================================
     print("Starting TV-MPC")
     # Initialized predictive model
     predictiveModel = PredictiveModel(n, d, map, 1)
     predictiveModel.addTrajectory(xPID_cl,uPID_cl)
-    #Initialize TV-MPC
+    # Initialize TV-MPC
     ltvmpcParam.timeVarying = True 
     mpc = MPC(ltvmpcParam, predictiveModel)
     # Run closed-loop sim
     xTVMPC_cl, uTVMPC_cl, xTVMPC_cl_glob, _ = simulator.sim(xS, mpc)
     print("===== TV-MPC terminated")
 
-    # ======================================================================================================================
-    # ==============================  LMPC w\ LOCAL LINEAR REGRESSION ======================================================
-    # ======================================================================================================================
+    # ==================================================================================================================
+    # ==============================  LMPC w\ LOCAL LINEAR REGRESSION ==================================================
+    # ==================================================================================================================
     print("Starting LMPC")
     # Initialize Predictive Model for lmpc
     lmpcpredictiveModel = PredictiveModel(n, d, map, 4)
-    for i in range(0,4): # add trajectories used for model learning
+    for i in range(0, 4):  # add trajectories used for model learning
         lmpcpredictiveModel.addTrajectory(xPID_cl,uPID_cl)
 
     # Initialize Controller
-    lmpcParameters.timeVarying     = True 
+    lmpcParameters.timeVarying = True
     lmpc = LMPC(numSS_Points, numSS_it, QterminalSlack, lmpcParameters, lmpcpredictiveModel)
-    for i in range(0,4): # add trajectories for safe set
-        lmpc.addTrajectory( xPID_cl, uPID_cl, xPID_cl_glob)
+    for i in range(0, 4):  # add trajectories for safe set
+        lmpc.addTrajectory(xPID_cl, uPID_cl, xPID_cl_glob)
     
     # Run sevaral laps
     for it in range(numSS_it, Laps):
@@ -116,15 +117,15 @@ def main():
         # Add trajectory to controller
         lmpc.addTrajectory( xLMPC, uLMPC, xLMPC_glob)
         # lmpcpredictiveModel.addTrajectory(np.append(xLMPC,np.array([xS[0]]),0),np.append(uLMPC, np.zeros((1,2)),0))
-        lmpcpredictiveModel.addTrajectory(xLMPC,uLMPC)
-        print("Completed lap: ", it, " in ", np.round(lmpc.Qfun[it][0]*dt, 2)," seconds")
+        lmpcpredictiveModel.addTrajectory(xLMPC, uLMPC)
+        print("Completed lap: ", it, " in ", np.round(lmpc.Qfun[it][0]*dt, 2), " seconds")
     print("===== LMPC terminated")
 
-    # # ======================================================================================================================
-    # # ========================================= PLOT TRACK =================================================================
-    # # ======================================================================================================================
+    # # ================================================================================================================
+    # # ========================================= PLOT TRACK ===========================================================
+    # # ================================================================================================================
     for i in range(0, lmpc.it):
-        print("Lap time at iteration ", i, " is ",np.round( lmpc.Qfun[i][0]*dt, 2), "s")
+        print("Lap time at iteration ", i, " is ", np.round(lmpc.Qfun[i][0]*dt, 2), "s")
 
     print("===== Start Plotting")
     plotTrajectory(map, xPID_cl, xPID_cl_glob, uPID_cl, 'PID')
@@ -140,5 +141,6 @@ def main():
     # animation_states(map, LMPCOpenLoopData, lmpc, Laps-2)
     # saveGif_xyResults(map, LMPCOpenLoopData, lmpc, Laps-2)
 
-if __name__== "__main__":
-  main()
+
+if __name__ == "__main__":
+    main()
